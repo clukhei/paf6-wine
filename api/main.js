dba372bc1417eff209be420aa607af6080db1df3
Lu Khei <kheiboardwarrior@gmail.com>
2020-12-15 09:58:32 +0800

    use promise.all to start mongo and mysql connections

--- api/main.js
+++ api/main.js
@@ -5,16 +5,25 @@ const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 const cors = require("cors");
 const { ObjectId } = require("mongodb");
+const mysql = require('mysql2/promise')
 
 const app = express();
 app.use(cors());
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(express.static(__dirname +"/paf6-wine-frontend"))
+app.use(express.static(__dirname + "/paf6-wine-frontend"));
 const PORT =
   parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
-
+  
+  const pool = mysql.createPool({
+    host: process.env.MYSQL_SERVER,
+    port: process.env.MYSQL_SVR_PORT,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_SCHEMA,
+    connectionLimit: process.env.MYSQL_CON_LIMIT,
+  });
 const MONGO_URL = "mongodb://localhost:27017";
 //same for the atlas connection
 
@@ -63,8 +72,8 @@ app.get("/wine/:id", async (req, res) => {
         title: 1,
         variety: 1,
         winery: 1,
-	  })
-	  .toArray()
+      })
+      .toArray();
     res.status(200);
     res.type("application/json");
     res.json(result);
@@ -99,13 +108,25 @@ app.use((req, res) => {
   res.redirect("/");
 });
 
-mongoClient
-  .connect()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`${PORT} started`);
-    });
-  })
-  .catch((e) => {
-    console.error("Cannot connect to mongodb", e);
-  });
+
+  const startSQL = pool
+    .getConnection()
+    .then((conn) => {
+      conn.ping();
+      console.log("pinged");
+      return conn
+    })
+    .then((conn) => conn.release());
+
+Promise.all([mongoClient.connect(), startSQL])
+  .then(()=> console.log(`Mongo and mysql started at PORT ${PORT}`));
+// mongoClient
+//   .connect()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`${PORT} started`);
+//     });
+//   })
+//   .catch((e) => {
+//     console.error("Cannot connect to mongodb", e);
+//   });
